c052d9b4fe34ee5603377f2d46561ad28a2052d9
weberliu1996 <53137892+weberliu1996@users.noreply.github.com>
2021-02-11 01:42:57 +1100

    fixed the QR

--- OSCEgen/generateQR.py
+++ OSCEgen/generateQR.py
@@ -4,7 +4,7 @@ import png
 def generate_link_code(base_url, osce_id):
     qr = pyqrcode.create(base_url + "/" + osce_id + "/prompt")
     qr.png('./public/' + osce_id + "/prompt_qr.png", scale = 10)
-    qr2 = pyqrcode.create(base_url + "/" + osce_id + "actor_prompt")
+    qr2 = pyqrcode.create(base_url + "/" + osce_id + "/actor_prompt")
     qr2.png('./public/' + osce_id + "/actor_prompt_qr.png", scale = 10)
     print("QR code generated for: " + osce_id)
     return
